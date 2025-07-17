debf0762b48b6cb2e329a9c9a7644cad4d0575f8
Scott Pierce <ddrscott@gmail.com>
2025-07-17 10:11:13 -0500

    Add mouse constraint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,42 @@
 import Phaser from 'phaser';
 import * as dat from 'dat.gui';
-import { gameConfig } from './config/gameConfig';
+import TowerScene from './scenes/TowerScene';
+import LevelsScene from './scenes/LevelsScene';
+import GamepadTest from './scenes/GamepadTest';
 
 // Make dat.GUI available globally for compatibility with the TestScene
 window.dat = dat;
 
 // Create and start the game
-export const game = new Phaser.Game(gameConfig);
+export const game = new Phaser.Game(
+{
+    type: Phaser.AUTO,
+    parent: 'game-container',
+    backgroundColor: '#232333',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: '100%',
+        height: '100%',
+    },
+    physics: {
+        default: 'matter',
+        matter: {
+            gravity: { y: 1 },
+            debug: {
+                showBody: true,
+                showStaticBody: true,
+                showVelocity: false,
+                bodyColor: 0xff0000
+            },
+            positionIterations: 20,
+            velocityIterations: 10,
+            constraintIterations: 2,
+        }
+    },
+    input: {
+        gamepad: true
+    },
+    scene: [TowerScene, LevelsScene, GamepadTest]
+}
+);
