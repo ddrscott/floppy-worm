7aac05b3687e693ea39abf8d4210528ba0464eca
Scott Pierce <ddrscott@gmail.com>
2025-08-12 14:13:47 -0500

    Allow all hosts for testing

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
         tsconfigPaths()
     ],
     server: {
-        port: 3001
+        port: 3001,
+        allowedHosts: true,
     },
     optimizeDeps: {
         include: ["phaser", "dat.gui"],
